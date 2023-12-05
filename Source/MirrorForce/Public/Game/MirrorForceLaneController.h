e56d40949b6d40e002e28818ae870f649d619f14
Future Wayne <futurewayne.ludens@gmail.com>
2023-12-05 15:15:27 -0700

    Adding winning condition

--- Source/MirrorForce/Public/Game/MirrorForceLaneController.h
+++ Source/MirrorForce/Public/Game/MirrorForceLaneController.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "MirrorForceLaneController.generated.h"
 
+class ATriggerBox;
+
 USTRUCT(BlueprintType)
 struct FLaneInfo
 {
@@ -15,7 +17,10 @@ struct FLaneInfo
 	AActor* LaneActor = nullptr;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lane Scroller")
-	AActor* Waypoint = nullptr;
+	AActor* AnchorPoint = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lane Scroller")
+	ATriggerBox* EndingTriggerBox = nullptr;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lane Scroller")
 	ACameraActor* Camera = nullptr;
@@ -44,6 +49,8 @@ protected:
 	TArray<FLaneInfo> Lanes = {};
 
 private:
+	void OnTriggerBoxOverlap(AActor* OverlappedActor, AActor* OtherActor);
+	
 	int CurrentLaneIndex = 0;
 
 	TArray<FVector> AnchorPointLocations = {};
